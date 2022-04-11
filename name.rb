f2c8b6013d2969cf7dba401722a142a2f44493be
vonhatcuong <vonhatcuongyy@gmail.com>
2022-04-11 10:25:00 +0700

    add full name

--- name.rb
+++ name.rb
@@ -1,4 +1,6 @@
-puts "What is your name?"
-name = gets.chomp
-10.times {|x| puts "Hello " + name}
+puts "What is your first name?"
+first_name = gets.chomp
+puts "What is your last name?"
+last_name = gets.chomp
+puts "your full name #{first_name} #{last_name}"
   
